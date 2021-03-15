4337469cf4ccae89fce6fd9e9696d8db8e31914e
Jon <Jon@ALPHONSE>
2021-03-14 22:55:40 -0400

    Initial commit just to demonstrate some CMake build system functionality. As it turns out, we can specify remote dependencies (e.g. github repos) that are downloaded at configure time. This means we don't have to excessively use git submodules and cruft up our extern folder.

---
+++ game/orcmeat.cpp
@@ -0,0 +1,13 @@
+// std
+#include <iostream>
+
+// extern
+#include<nlohmann/json.hpp>
+
+// game_library
+#include <lib.hpp>
+
+int main(void) {
+    print_welcome_message();
+    exit(EXIT_SUCCESS);
+}
---
+++ src/lib.cpp
@@ -0,0 +1,7 @@
+#include <lib.hpp>
+
+#include <iostream>
+
+void print_welcome_message(void) {
+    std::cout << "game\n";
+}
